bc1a1053cffc9d2334dae38ee5a275ac2a95d88d
Voyager162 <westonrgardner@gmail.com>
2024-09-23 15:22:21 -0700

    addded comments to basic while loop

--- whileLoops/UserInputLoop.py
+++ whileLoops/UserInputLoop.py
@@ -12,8 +12,8 @@ while running:
         print("it")
         number -= 1
 
-#First, the whole code is constantly repeated by a while loop that is always true
+# First, the whole code is constantly repeated by a while loop that is always true
 
-#Next, it takes in user input through a try and except statement that fails if the user did not input a number, and this is repeated until they can put in a number
+# Next, it takes in user input through a try and except statement that fails if the user did not input a number, and this is repeated until they can put in a number
 
-#finally, a while loop repeats it the amount of times that the user specified
+# finally, a while loop repeats it the amount of times that the user specified
