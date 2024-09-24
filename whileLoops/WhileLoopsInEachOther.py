89ec7ad4ef2713afe4db7263b4072167b7129488
Voyager162 <westonrgardner@gmail.com>
2024-09-24 12:25:48 -0700

    added while loops inside each other

---
+++ whileLoops/WhileLoopsInEachOther.py
@@ -0,0 +1,19 @@
+i = 3
+j = 4
+while i > 0:
+    print("apple")
+    while j > 0:
+        print("banana")
+        j -= 1
+    i -= 1
+    j = 4
+
+# the whole code runs three times because i = 3
+
+# in the code it print's out apple then enters the j loop
+
+# in the j loop it prints banana and reduces j by 1
+
+# j loop will run 4 times
+
+# then at the end of i loop i goes down by one and it brings j back up to 4
